75412fccb7c2f22ea645c49a59e5c89111fd279e
Nicolas Barboteu <nicolas.barboteu@etu.toulouse-inp.fr>
2024-10-21 15:22:12 +0200

    tp2 : 21/10/24

--- tp2/tp.c
+++ tp2/tp.c
@@ -1,17 +1,48 @@
 /* GPLv2 (c) Airbus */
 #include <debug.h>
+#include <intr.h>
 
-void bp_handler() {
-   // TODO
+void bp_handler_c() {
+	debug("\nBreakpoint Exception (#BP)\n");
+	uint32_t val;
+	asm volatile ("mov 4(%%ebp), %0":"=r"(val));
+	debug("Valeur EBP-4: %p\n\n", (void *)val);
+	/* Dernière instruction : RET
+		Cette instruction permet de quitter une procédure. 
+		Ainsi, elle indique au microprocesseur 
+		qu'il doit désempiler la valeur du pointeur d'instructions 
+		contenu dans la pile et la copier dans les registres CS:IP.
+		Ensuite, l'exécution du code se poursuit 
+		à l'instruction suivant de l'instruction «CALL».
+    */
 }
 
+void bp_handler() {
+	asm volatile(
+		"pusha\n"				// Sauvegarde tous les registres généraux
+		"call bp_handler_c\n"	// Appeler la fonction de gestion d'interruption
+		"popa\n"				// Restaurer tous les registres généraux
+		"leave\n"				// Restaure le pointeur de pile (ESP) et le pointeur de base (EBP) à leurs valeurs d'avant l'appel de la fonction.
+		"iret\n"				// Retourner de l'interruption
+	);
+}
+
+
 void bp_trigger() {
-	// TODO
+	asm volatile("int3");
+	debug("finished #BP handler processing\n");
 }
 
 void tp() {
-	// TODO print idtr
+	idt_reg_t idtr;
+	get_idtr(idtr);
 
-	// TODO call bp_trigger
-   //bp_trigger();
+	printf("\nIDT Base Address: %p\n", (void *)idtr.addr);
+
+	int_desc_t *idt;
+	idt = (int_desc_t *)idtr.addr;
+
+	build_int_desc(&idt[3], gdt_krn_seg_sel(1), (offset_t)bp_handler);
+
+	bp_trigger();
 }
